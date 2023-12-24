238a3900d70e38a9442c1275053faafe0bb98b27
debian <dario@dariodbb.it>
2023-12-24 17:57:07 +0100

    deploy

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 let wrapper = document.querySelector('#wrapper');
-
 fetch('meteo.php')
-    .then(response => response.json())
-    .then(previsioni => {
-        view(previsioni);
+    .then(response => {
+        response.json()
+            .then(previsioni => {
+
+                view(previsioni);
+
+            })
+
     })
-    .catch(error => {
-        console.error('Errore nel recupero dei dati:', error);
-    });
 
 function view(previsioni) {
-    // Inverto l'array delle previsioni
-    const previsioniReversed = previsioni.reverse();
 
-    // Itero sulle previsioni e creo le righe della tabella
+    previsioniReversed = previsioni.reverse();
+
     previsioniReversed.forEach(previsione => {
+
         let row = document.createElement('div');
-        row.classList.add('row', 'align-items-center');
+
         row.innerHTML = `
-            <div class="col">${previsione[0]}</div>
-            <div class="col">${previsione[1]}</div>
-            <div class="col">${previsione[2]}</div>
-        `;
+<div class="row align-items-center">
+<div class="col">${previsione[2]}</div>
+<div class="col">${previsione[1]}</div>
+<div class="col">${previsione[0]}</div>
+</div>
+
+`
         wrapper.appendChild(row);
+
     });
+
+
 }
