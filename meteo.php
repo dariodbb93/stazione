4b1f720bba7021b29e3515725016193cf62d14cc
debian <dario@dariodbb.it>
2023-12-24 17:10:33 +0100

    deploy

--- meteo.php
+++ meteo.php
@@ -6,7 +6,7 @@ $dbname = 'meteo';
 $user = 'dario';
 $password = 'HotDingo627';
 $connection = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
-$query = "SELECT * FROM previsioni";
+$query = "SELECT * FROM previsioni GROUP BY tramonto";
 $result = pg_query($connection, $query);
 $data = array();
 
